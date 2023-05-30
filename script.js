3445ee763309b0960a0d9060c64803a4f2b2c351
yakovlevaElena <elena94yakovleva@gmail.com>
2023-05-30 16:55:33 +0600

    theme dark

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-
+//делаем кнопку
 document.getElementById('hider').onclick = function() {
     const cards = document.querySelectorAll('#card-hidden')
     for (const card of cards) {
         card.hidden = !card.hidden
     }
+  }
+
+//делаем тему
+let changeThemeButtons = document.querySelectorAll('.theme-toogle');
+document.getElementById('dark').onclick = function() {
+    
   }

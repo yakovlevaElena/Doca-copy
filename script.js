d9cd6a1b2c89300e85121073429344ada8938142
yakovlevaElena <elena94yakovleva@gmail.com>
2023-07-01 15:46:49 +0600

    corrected theme classes

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-//делаем кнопку
-document.getElementById('hider').onclick = function() {
-    const cards = document.querySelectorAll('#card-hidden')
-    for (const card of cards) {
-        card.hidden = !card.hidden
-    }
-  }
+// //делаем кнопку
+// document.getElementById('hider').onclick = function() {
+//     const cards = document.querySelectorAll('#card-hidden')
+//     for (const card of cards) {
+//         card.hidden = !card.hidden
+//     }
+//   }
 
-//делаем тему
-const themeToggle = document.querySelectorAll(.'theme');
+// //делаем тему
+// const themeToggle = document.querySelectorAll(.'theme');
 
-themeToggle.forEach()
+// themeToggle.forEach()
+
+let changeThemeButtons = document.querySelectorAll('.changeTheme'); // Помещаем кнопки смены темы в переменную
+
+changeThemeButtons.forEach(button => {
+    button.addEventListener('click', function () { // К каждой добавляем обработчик событий на клик
+        let theme = this.dataset.theme; // Помещаем в переменную название темы из атрибута data-theme
+        applyTheme(theme); // Вызываем функцию, которая меняет тему и передаем в нее её название
+    });
+});
+
+function applyTheme(themeName) {
+    document.querySelector('[title="theme"]').setAttribute('href', `css/theme-${themeName}.css`); // Помещаем путь к файлу темы в пустой link в head
+    changeThemeButtons.forEach(button => {
+        button.style.display = 'block'; // Показываем все кнопки смены темы
+    });
+    document.querySelector(`[data-theme="${themeName}"]`).style.display = 'none'; // Но скрываем кнопку для активной темы
+}

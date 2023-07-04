7eadf9b14a77beb88ab5ee5ad94a361cd9efe84f
yakovlevaElena <elena94yakovleva@gmail.com>
2023-07-04 15:31:24 +0600

    themes

--- script.js
+++ script.js
@@ -11,19 +11,32 @@
 
 // themeToggle.forEach()
 
-let changeThemeButtons = document.querySelectorAll('.changeTheme'); // Помещаем кнопки смены темы в переменную
 
-changeThemeButtons.forEach(button => {
-    button.addEventListener('click', function () { // К каждой добавляем обработчик событий на клик
-        let theme = this.dataset.theme; // Помещаем в переменную название темы из атрибута data-theme
-        applyTheme(theme); // Вызываем функцию, которая меняет тему и передаем в нее её название
-    });
-});
+// Пробую изменить тему по методу из youtube
+// let changeThemeButtons = document.querySelectorAll('.changeTheme'); // Помещаем кнопки смены темы в переменную
 
-function applyTheme(themeName) {
-    document.querySelector('[title="theme"]').setAttribute('href', `css/theme-${themeName}.css`); // Помещаем путь к файлу темы в пустой link в head
-    changeThemeButtons.forEach(button => {
-        button.style.display = 'block'; // Показываем все кнопки смены темы
-    });
-    document.querySelector(`[data-theme="${themeName}"]`).style.display = 'none'; // Но скрываем кнопку для активной темы
-}
+// changeThemeButtons.forEach(button => {
+//     button.addEventListener('click', function () { // К каждой добавляем обработчик событий на клик
+//         let theme = this.dataset.theme; // Помещаем в переменную название темы из атрибута data-theme
+//         applyTheme(theme); // Вызываем функцию, которая меняет тему и передаем в нее её название
+//     });
+// });
+
+// function applyTheme(themeName) {
+//     document.querySelector('[title="theme"]').setAttribute('href', `css/theme-${themeName}.css`); // Помещаем путь к файлу темы в пустой link в head
+//     changeThemeButtons.forEach(button => {
+//         button.style.display = 'block'; // Показываем все кнопки смены темы
+//     });
+//     document.querySelector(`[data-theme="${themeName}"]`).style.display = 'none'; // Но скрываем кнопку для активной темы
+// }
+
+//Пробую изменить тему по методу htmlacademy
+document.querySelector('.page');
+document.querySelector('.page').classList.add('theme-dark');
+
+const button= document.querySelector('.dsafa')
+
+button.addEventListener('click', () => {
+
+})
+
